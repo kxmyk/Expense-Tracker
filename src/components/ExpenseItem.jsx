10787d19293943556ce9a7e160c4a7172dd10316
Kamil Lukasiuk <kamilplgt@gmail.com>
2023-04-06 19:18:53 +0200

    implemented ExpenseDate component

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,14 @@
+import ExpenseDate from "./ExpenseDate.jsx";
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2023, 4, 6);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 284;
+function ExpenseItem(props) {
     return (
         <>
             <div className="expense-item">
-                <div>{expenseDate.toISOString().slice(0, 10)}</div>
+                <ExpenseDate date={props.date}/>
                 <div className="expense-item__description">
-                    <h2>{expenseTitle}</h2>
-                    <div className="expense-item__price">€{expenseAmount}</div>
+                    <h2>{props.title}</h2>
+                    <div className="expense-item__price">€{props.amount}</div>
                 </div>
             </div>
         </>
