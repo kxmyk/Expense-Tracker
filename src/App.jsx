ca2fabe7b74e6957dda52b4de8bd68cc05aa26e8
Kamil Lukasiuk <kamilplgt@gmail.com>
2023-04-07 18:27:37 +0200

    implemented ExpensesFilter and Charts components

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
-import './App.css'
-import Expenses from "./components/Expenses/Expenses.jsx"
-import NewExpense from "./components/NewExpense/NewExpense.jsx"
+import {useState} from 'react';
+
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const existing_expenses = [
+	{id: 'e1', title: 'Toilet Paper', amount: 94, date: new Date(2021, 7, 14)},
+	{id: 'e2', title: 'New TV', amount: 799, date: new Date(2021, 2, 12)},
+	{id: 'e3', title: 'Car Insurance', amount: 294, date: new Date(2023, 2, 28)},
+	{id: 'e4', title: 'New Desk (Wooden)', amount: 450, date: new Date(2022, 5, 12)},
+	{id: 'e5', title: 'New MacBook', amount: 2399, date: new Date(2023, 1, 8)},
+	{id: 'e6', title: 'Roof Repair', amount: 400, date: new Date(2022, 12, 1)},
+]
 
 const App = () => {
-    const expenses = [
-        {id: 'e1', title: 'Toilet Paper', amount: 94.12, date: new Date(2020, 7, 14)},
-        {id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)},
-        {id: 'e3', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
-        {id: 'e4', title: 'New Desk (Wooden)', amount: 450, date: new Date(2021, 5, 12)}
-    ]
+	const [expenses, setExpenses] = useState(existing_expenses);
 
-    const addExpenseHandler = expense => {
-        console.log('in App.js')
-        console.log(expense)
-    }
+	const addExpenseHandler = (expense) => {
+		setExpenses((prevExpenses) => {
+			return [expense, ...prevExpenses];
+		});
+	};
 
-    return (
-        <div>
-            <NewExpense onAddExpense={addExpenseHandler}/>
-            <Expenses items={expenses}/>
-        </div>
-    )
-}
+	return (
+		<>
+			<NewExpense onAddExpense={addExpenseHandler}/>
+			<Expenses items={expenses}/>
+		</>
+	);
+};
 
-export default App
+export default App;
--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,28 +1,21 @@
-import {useState} from "react"
+import React from 'react';
 
-import Card from "../UI/Card.jsx"
-import ExpenseDate from "./ExpenseDate.jsx"
-
-import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title)
+	return (
+		<li>
+			<Card className='expense-item'>
+				<ExpenseDate date={props.date}/>
+				<div className='expense-item__description'>
+					<h2>{props.title}</h2>
+					<div className='expense-item__price'>€{props.amount}</div>
+				</div>
+			</Card>
+		</li>
+	);
+};
 
-    const clickHandler = () => {
-        setTitle('Updated')
-    }
-
-    return (
-        <Card className="expense-item">
-            <ExpenseDate date={props.date}/>
-            <div className="expense-item__description">
-                <h2>{title}</h2>
-                <div className="expense-item__price">€{props.amount}</div>
-            </div>
-            <button onClick={clickHandler}>Change Title
-            </button>
-        </Card>
-    )
-}
-
-export default ExpenseItem
+export default ExpenseItem;
